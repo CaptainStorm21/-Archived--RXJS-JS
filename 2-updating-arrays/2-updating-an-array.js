98bf61c68a153656dba8128083e803c0129a0d65
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-04 23:19:14 -0800

    Create 2-updating-an-array.js

---
+++ 2-updating-arrays/2-updating-an-array.js
@@ -0,0 +1,14 @@
+//old function
+
+const numbers = [1, 2, 4]
+
+function double(number){
+  return number * 2;
+}
+
+//map function 
+//array.map(fn)
+
+const doubledNumbers = numbers.map(double);
+
+console.log(doubledNumbers)
